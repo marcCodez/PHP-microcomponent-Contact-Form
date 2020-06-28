a30c87d0d751a4c944c05f545b429268654aae9c
marcCodez <marc_pulumbarit@hotmail.com>
2020-06-28 18:00:28 +1000

    send email redo

--- index.php
+++ index.php
@@ -19,7 +19,37 @@
                 $msgClass = 'alert-danger';
             } else {
                 // Passed
-                echo 'PASSED';
+                // Recipient Email
+                $toEmail = 'marc_pulumbarit@hotmail.com';
+                $tosubject = 'Contact Request From '.$name;
+                $body = '<h2>Contact Request</h2>
+                <h4>Name</h4><p>'.$name.'</p>
+                <h4>Email</h4><p>'.$email.'</p>
+                <h4>Message</h4><p>'.$message.'</p>
+                ';
+
+                // Multipurpose Internet Mail Extension is an internet standard, encoded file format used by email programs. 
+                // \r = Carriage return means to return to the beginning of the current line without advancing downward.
+                // \n = new line
+                // .= means append not overwrite the variable
+                // Email Headers
+                $headers = "MIME-Version: 1.0" ."\r\n";
+                $headers .= "Content-Type:text/html;charset=UTF-8" . "\r\n";
+
+                // Additional Headers
+                $headers .= "From: " .$name. "<".$email.">". "\r\n";
+
+                // Use mail function in if statement to say it sends or it doesnt
+                if(mail($toEmail, $subject, $body, $headers)){
+                    // Email Sent
+                    $msg = 'Your email has been sent';
+                    $msgClass = 'alert-success';
+                } else {
+                    $msg = 'Your email was not sent';
+                    $msgClass = 'alert-danger';
+
+                }
+
             }
         } else {
             // Failed
@@ -54,19 +84,15 @@
             <label>Name</label>
     <!-- set the value so the value will remain on the form if theres an error -->
     <!-- if post name is set output the value if not output nothing, using ternary -->
-            <input type="text" name="name" class="form-control" 
-            value="<?php echo isset($_POST['name']) ? $name: ''; ?>
-            ">
+            <input type="text" name="name" class="form-control" value="<?php echo isset($_POST['name']) ? $name: ''; ?>">
         </div>
         <div class="form-group">
         <label>Email</label>
-        <input type="text" name="email" class="form-control" 
-        value="<?php echo isset($_POST['email']) ? $email: ''; ?>">
+        <input type="text" name="email" class="form-control" value="<?php echo isset($_POST['email']) ? $email: ''; ?>">
         </div>
         <div class="form-group">
         <label>Message</label>
-        <textarea name="message" class="form-control">
-        <?php echo isset($_POST['message']) ? $message: ''; ?></textarea>
+        <textarea name="message" class="form-control"><?php echo isset($_POST['message']) ? $message: ''; ?></textarea>
         </div>
         <br>
         <button type="submit" name="submit" class="btn btn-primary">Submit</button>
